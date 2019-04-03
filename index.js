aafadcee01cd704571bca7b84ce661b780f21079
Paweł Kopycki <pawel.kopycki@gmail.com>
2019-04-03 10:27:32 +0200

    add error about a range

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ let _change = (arrayOfdigits) => {
   const hundreds = ["", "sto ", "dwieście ", "trzysta ", "czterysta ", "pięćset ", "sześćset ", "siedemset ", "osiemset ", "dziewięćset "];
   const lastDigits = arrayOfdigits[arrayOfdigits.length - 2] + arrayOfdigits[arrayOfdigits.length - 1];
   let words = [dozens, hundreds];
-  let x = [];
+  let newArrOfDigits = [];
 
   if (lastDigits > 10 && lastDigits < 20) {
-    x = arrayOfdigits.slice(0, arrayOfdigits.length - 2);
-    x.push(0, arrayOfdigits[arrayOfdigits.length - 1]);
+    newArrOfDigits = arrayOfdigits.slice(0, arrayOfdigits.length - 2);
+    newArrOfDigits.push(0, arrayOfdigits[arrayOfdigits.length - 1]);
     words.unshift(teen);
   } else {
-    x = arrayOfdigits;
+    newArrOfDigits = arrayOfdigits;
     words.unshift(unity);
   }
  
   let str = '';
 
-  for (let i = 0; i < x.length; i++) {
-    str += words[x.length - 1 - i][x[i]];
+  for (let i = 0; i < newArrOfDigits.length; i++) {
+    str += words[newArrOfDigits.length - 1 - i][newArrOfDigits[i]];
   }
 
   return str;
@@ -29,22 +29,23 @@ let _change = (arrayOfdigits) => {
 let change = (number) => {
   if (isNaN(number)) throw new TypeError('This is not a number');
   if (!Number.isInteger(number)) throw new TypeError('This is not a integer number');
+  if (number > 999999 || number < 0) throw new Error('This version works only in range 0 - 999999')
   let arrayOfdigits = number.toString().split('');
   const thousands = ["", "tysiąc ", "tysiące ", "tysięcy "];
   let str = '';
-  let suffix = '';
+  let suffinewArrOfDigits = '';
   if (number > 999) {
     let t = Math.floor(number / 1000);
     if (t == 1) {
-      suffix = thousands[1];
+      suffinewArrOfDigits = thousands[1];
     } else if (t % 100 > 11 && t % 100 < 21) {
-      suffix = thousands[3];
+      suffinewArrOfDigits = thousands[3];
     } else if ((t % 100 > 1 && t % 100 < 5) || (t % 10 > 1 && t % 10 < 5)) {
-      suffix = thousands[2];
+      suffinewArrOfDigits = thousands[2];
     } else {
-      suffix = thousands[3];
+      suffinewArrOfDigits = thousands[3];
     }
-    str += _change(arrayOfdigits.slice(0, arrayOfdigits.length - 3)) + suffix + _change(arrayOfdigits.slice(arrayOfdigits.length - 3));
+    str += _change(arrayOfdigits.slice(0, arrayOfdigits.length - 3)) + suffinewArrOfDigits + _change(arrayOfdigits.slice(arrayOfdigits.length - 3));
   } else if (number == 0) {
     str = 'zero';
   } else {
@@ -53,4 +54,5 @@ let change = (number) => {
   return str;
 }
 
-module.exports = change;
+console.log(change(1))
+module.enewArrOfDigitsports = change;
